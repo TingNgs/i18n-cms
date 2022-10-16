b73f47ebfce64b08d381b909f2f7b6b78fa4c828
TingNg <xnxgxsx@hotmail.com>
2022-10-16 16:14:21 +0800

    Add rwd delete key modal

--- src/page/Repo/Table/component/ActionCell/DeleteBtn.tsx
+++ src/page/Repo/Table/component/ActionCell/DeleteBtn.tsx
@@ -11,10 +11,12 @@ import {
   Text,
   PopoverBody,
   PopoverFooter,
-  useDisclosure
+  useDisclosure,
+  useBreakpointValue
 } from '@chakra-ui/react';
 import { memo, useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
+import DeleteModal from '../../../../../component/DeleteModal';
 import { removeLocaleOnIndex } from '../../../../../redux/editingRepoSlice';
 import { useAppDispatch } from '../../../../../redux/store';
 
@@ -26,6 +28,7 @@ const DeleteBtn = ({
   localeKey?: string;
   index?: number;
 }) => {
+  const isLg = useBreakpointValue({ base: false, lg: true });
   const dispatch = useAppDispatch();
   const { isOpen, onOpen, onClose } = useDisclosure();
   const { t } = useTranslation();
@@ -38,39 +41,69 @@ const DeleteBtn = ({
   }, [index]);
 
   return (
-    <Popover placement="left" onOpen={onOpen} isOpen={isOpen} onClose={onClose}>
-      <PopoverTrigger>
-        <IconButton
-          aria-label="more"
-          icon={<DeleteIcon />}
-          colorScheme="gray"
-        />
-      </PopoverTrigger>
-      <Portal>
-        <PopoverContent>
-          <PopoverArrow />
-          <PopoverBody>
-            <Text
-              dangerouslySetInnerHTML={{
-                __html: repoT('Delete confirmation', {
-                  localeKey
-                })
-              }}
-            />
-          </PopoverBody>
-          <PopoverFooter>
-            <ButtonGroup size="sm">
-              <Button variant="outline" onClick={onClose}>
-                {t('Cancel')}
-              </Button>
-              <Button colorScheme="red" onClick={onDeleteBtnClicked}>
-                {t('Delete')}
-              </Button>
-            </ButtonGroup>
-          </PopoverFooter>
-        </PopoverContent>
-      </Portal>
-    </Popover>
+    <>
+      <Popover
+        placement="left"
+        onOpen={onOpen}
+        isOpen={isLg && isOpen}
+        onClose={onClose}>
+        <PopoverTrigger>
+          <IconButton
+            aria-label="more"
+            icon={<DeleteIcon />}
+            colorScheme="gray"
+          />
+        </PopoverTrigger>
+        <Portal>
+          <PopoverContent>
+            <PopoverArrow />
+            <PopoverBody>
+              <Text
+                dangerouslySetInnerHTML={{
+                  __html: repoT('Delete key confirmation', {
+                    localeKey
+                  })
+                }}
+              />
+            </PopoverBody>
+            <PopoverFooter>
+              <ButtonGroup size="sm">
+                <Button variant="outline" onClick={onClose}>
+                  {t('Cancel')}
+                </Button>
+                <Button colorScheme="red" onClick={onDeleteBtnClicked}>
+                  {t('Delete')}
+                </Button>
+              </ButtonGroup>
+            </PopoverFooter>
+          </PopoverContent>
+        </Portal>
+      </Popover>
+
+      <DeleteModal
+        title={
+          <Text
+            dangerouslySetInnerHTML={{
+              __html: repoT('Delete key', {
+                localeKey
+              })
+            }}
+          />
+        }
+        content={
+          <Text
+            dangerouslySetInnerHTML={{
+              __html: repoT('Delete key confirmation', {
+                localeKey
+              })
+            }}
+          />
+        }
+        onClose={onClose}
+        onConfirm={onDeleteBtnClicked}
+        isOpen={isOpen && !isLg}
+      />
+    </>
   );
 };
 
